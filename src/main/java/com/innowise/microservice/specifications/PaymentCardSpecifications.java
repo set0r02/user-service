ae40b2bfbfc3f6a611e90c59f3721887d08c64ed
setor <marik123456789110011@gmail.com>
2026-05-22 00:32:36 +0300

    feet: added specifications for dao layer

---
+++ src/main/java/com/innowise/microservice/specifications/PaymentCardSpecifications.java
@@ -0,0 +1,32 @@
+package com.innowise.microservice.specifications;
+
+import com.innowise.microservice.model.PaymentCard;
+import com.innowise.microservice.model.User;
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+
+public class PaymentCardSpecifications {
+
+    public static Specification<PaymentCard> hasFirstName(String firstName){
+        return (root, query, criteriaBuilder) -> {
+            if (firstName == null || firstName.isBlank()){
+                return null;
+            }
+            Join<PaymentCard,User> paymentCardUserJoin = root.join("user");
+            return criteriaBuilder.like(criteriaBuilder.lower(paymentCardUserJoin.get("name")),firstName.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<PaymentCard> hasSurname(String surname){
+        return (root, query, criteriaBuilder) -> {
+            if(surname == null || surname.isBlank()) {
+                return null;
+            }
+            Join<PaymentCard,User> paymentCardUserJoin = root.join("user");
+            return criteriaBuilder.like(criteriaBuilder.lower(paymentCardUserJoin.get("surname")), surname + "%");
+        };
+    }
+
+
+
+}

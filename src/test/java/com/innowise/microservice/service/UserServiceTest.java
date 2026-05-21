4befdb12bfbfceff9376551a5f259eac22fb28e7
setor <marik123456789110011@gmail.com>
2026-05-22 02:35:20 +0300

    feet: findUserByIdTest and getAllUsersTest

--- src/test/java/com/innowise/microservice/service/UserServiceTest.java
+++ src/test/java/com/innowise/microservice/service/UserServiceTest.java
@@ -10,12 +10,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
@@ -68,8 +77,8 @@ public class UserServiceTest {
         UserOutputDto actualResponse = userService.createUser(userInputDto);
 
         assertNotNull(actualResponse);
-        assertEquals(actualResponse.id(),1L);
-        assertEquals(actualResponse.email(),"petrov@mail.com");
+        assertEquals(1L, actualResponse.id());
+        assertEquals("petrov@mail.com", actualResponse.email());
 
         verify(userRepository, times(1)).findByEmail(userInputDto.email());
         verify(userRepository, times(1)).save(user);
@@ -77,12 +86,60 @@ public class UserServiceTest {
 
     @Test
     void findUserByIdTest(){
+        Long userId = 1L;
+        User user = new User();
+        UserOutputDto userOutputDto = new UserOutputDto(
+                1L,
+                "Petr",
+                "Petrov",
+                LocalDate.of(2004, 5, 7),
+                "petrov@mail.com",
+                true,
+                null,
+                null,
+                null);
 
+        when(userRepository.findById(userId))
+                .thenReturn(Optional.of(user));
+        when(userMapper.toDto(user))
+                .thenReturn(userOutputDto);
+
+        UserOutputDto actualDto = userService.findUserById(userId);
+        assertNotNull(actualDto);
+        assertEquals(userId, actualDto.id());
     }
+
 
     @Test
     void getAllUsersTest(){
+        String name = "Ivan";
+        String surname = "Ivanov";
+        Pageable pageable = PageRequest.of(0, 10);
 
+        User user = new User();
+        Page<User> userPage = new PageImpl<>(List.of(user));
+        UserOutputDto userOutputDto = new UserOutputDto(
+                1L,
+                "Petr",
+                "Petrov",
+                LocalDate.of(2004, 5, 7),
+                "petrov@mail.com",
+                true,
+                null,
+                null,
+                null);
+
+        when(userRepository.findAll(any(Specification.class), eq(pageable)))
+                .thenReturn(userPage);
+        when(userMapper.toDto(user))
+                .thenReturn(userOutputDto);
+
+        Page<UserOutputDto> actualPage = userService.getAllUsers(name, surname, pageable);
+
+        assertNotNull(actualPage);
+        assertThat(actualPage.getContent()).hasSize(1);
+        assertEquals(1L, actualPage.getContent().get(0).id());
+        verify(userRepository, times(1)).findAll(any(Specification.class), eq(pageable));
     }
 
     @Test

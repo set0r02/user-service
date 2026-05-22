4bb828225cb3eea9a0604792adf688cf75f9e8ef
setor <marik123456789110011@gmail.com>
2026-05-23 01:08:53 +0300

    feet: fix jar artifact pipeline

--- src/main/java/com/innowise/microservice/BookstoreApplication.java
+++ src/main/java/com/innowise/microservice/BookstoreApplication.java
@@ -2,7 +2,6 @@ package com.innowise.microservice;
 
 
 
-import com.innowise.microservice.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,7 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class BookstoreApplication {
-    static void main() {
-        var context = SpringApplication.run(BookstoreApplication.class);
+    static void main(String[] args) {
+        var context = SpringApplication.run(BookstoreApplication.class, args);
     }
 }

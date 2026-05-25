8c214f82a610219771e024e3a98f58f1394f5223
setor <marik123456789110011@gmail.com>
2026-05-25 13:51:42 +0300

    feet: remove comments

--- src/main/java/com/innowise/userservice/controller/PaymentCardController.java
+++ src/main/java/com/innowise/userservice/controller/PaymentCardController.java
@@ -25,14 +25,12 @@ public class PaymentCardController {
     public ResponseEntity<PaymentCardOutputDto> createPaymentCard(@RequestBody @Valid PaymentCardInputDto paymentCardInputDto){
         PaymentCardOutputDto paymentCardOutputDtoCreated = paymentCardService.createPaymentCard(paymentCardInputDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(paymentCardOutputDtoCreated);
-        //return new ResponseEntity<>(paymentCardOutputDtoCreated,HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PaymentCardOutputDto> findPaymentCardById(@PathVariable Long id){
         PaymentCardOutputDto paymentCardOutputDto = paymentCardService.findPaymentCardById(id);
         return ResponseEntity.ok(paymentCardOutputDto);
-        //return new ResponseEntity<>(paymentCardOutputDto,HttpStatus.OK);
     }
 
     @GetMapping
@@ -42,35 +40,30 @@ public class PaymentCardController {
             Pageable pageable){
         Page<PaymentCardOutputDto> paymentCardOutputDtoPage = paymentCardService.getAllPaymentCards(firstName,surname,pageable);
         return ResponseEntity.ok(paymentCardOutputDtoPage);
-        //return new ResponseEntity<>(paymentCardService.getAllPaymentCards(firstName,surname,pageable),HttpStatus.OK);
     }
 
     @GetMapping("/user/{id}")
     public ResponseEntity<List<PaymentCardOutputDto>> findAllPaymentCardsByUserId(@PathVariable(name = "id") Long userId){
         List<PaymentCardOutputDto> paymentCardOutputDtoListByUserId = paymentCardService.findAllPaymentCardsByUserId(userId);
         return ResponseEntity.ok(paymentCardOutputDtoListByUserId);
-        //return new ResponseEntity<>(paymentCardOutputDtoListByUserId,HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<PaymentCardOutputDto> updatePaymentCardById(@PathVariable Long id,@RequestBody @Valid PaymentCardInputDto paymentCardInputDto){
         PaymentCardOutputDto paymentCardOutputDtoUpdatedById = paymentCardService.updatePaymentCardById(id,paymentCardInputDto);
         return ResponseEntity.ok(paymentCardOutputDtoUpdatedById);
-        //return new ResponseEntity<>(paymentCardService.updatePaymentCardById(id,paymentCardInputDto),HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/status/{active}")
     public ResponseEntity<Void> updateCardPaymentStatus(@PathVariable Long id,@PathVariable Boolean active){
         paymentCardService.updateCardPaymentStatus(id,active);
         return ResponseEntity.noContent().build();
-        //return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePaymentCard(@PathVariable Long id){
         paymentCardService.deletePaymentCard(id);
         return ResponseEntity.noContent().build();
-        //return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
--- src/main/java/com/innowise/userservice/controller/UserController.java
+++ src/main/java/com/innowise/userservice/controller/UserController.java
@@ -22,14 +22,12 @@ public class UserController {
     public ResponseEntity<UserOutputDto> createUser(@RequestBody @Valid UserInputDto userInputDto){
         UserOutputDto userOutputDtoCreated = userService.createUser(userInputDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(userOutputDtoCreated);
-        //return new ResponseEntity<>(userOutputDtoCreated, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserOutputDto> findUserById (@PathVariable Long id){
         UserOutputDto userOutputDtoById = userService.findUserById(id);
         return ResponseEntity.ok(userOutputDtoById);
-        //return new ResponseEntity<>(userOutputDtoById,HttpStatus.OK);
     }
 
     @GetMapping()
@@ -40,21 +38,18 @@ public class UserController {
     ){
         Page<UserOutputDto> userOutputDtoPage = userService.getAllUsers(firstName,surname,pageable);
         return ResponseEntity.ok(userOutputDtoPage);
-        //return new ResponseEntity<>(userService.getAllUsers(firstName,surname,pageable),HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserOutputDto> updateUserById(@PathVariable Long id,@RequestBody @Valid UserInputDto userInputDto){
         UserOutputDto userOutputDtoUpdatedById = userService.updateUserById(id,userInputDto);
         return ResponseEntity.ok(userOutputDtoUpdatedById);
-        //return new ResponseEntity<>(userOutputDtoUpdatedById,HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/status/{active}")
     public ResponseEntity<Void> updateUserStatus(@PathVariable Long id, @PathVariable boolean active){
         userService.updateUserStatus(id,active);
         return ResponseEntity.noContent().build();
-        //return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
@@ -62,7 +57,6 @@ public class UserController {
     public ResponseEntity<Void> deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
-        //return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
